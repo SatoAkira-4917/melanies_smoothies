2b11b17cc79d5db25e3bf970953fd97ced44f27b
SatoAkira-4917 <sato.akira.4917@members.co.jp>
2024-10-31 15:32:29 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-#streamlit.title('My Parents New Healthy Diner')
 from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
