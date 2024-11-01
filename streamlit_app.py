1b24b4a166ec9cec4cb018cd57efcda0b9d1f162
SatoAkira-4917 <sato.akira.4917@members.co.jp>
2024-11-01 10:35:14 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ ingredients_list = st.multiselect(
 
 if ingredients_list:
     #st.write(ingredients_list)
-    #st.text(ingredients_list)
+    st.text(ingredients_list)
 
     ingredients_string = ''
 
@@ -45,8 +45,8 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
         
-        #search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
 
         st.subheader (fruit_chosen + ' Nutrition Information')
