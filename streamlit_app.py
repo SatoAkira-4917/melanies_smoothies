a866bc3fd0b5eb7e5cc95593e47eea42ced6635a
SatoAkira-4917 <sato.akira.4917@members.co.jp>
2024-11-01 10:44:53 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,10 @@ session = cnx. session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
+pd_df=my_dataframe. to_pandas()
+#st.dataframe (pd_df)
+#st.stop()
+
 
 ingredients_list = st.multiselect(
     'Choose up to5 ingredients:'
@@ -36,10 +40,6 @@ if ingredients_list:
     st.text(ingredients_list)
 
     ingredients_string = ''
-
-    pd_df=my_dataframe. to_pandas()
-    st.dataframe (pd_df)
-    st.stop()
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
